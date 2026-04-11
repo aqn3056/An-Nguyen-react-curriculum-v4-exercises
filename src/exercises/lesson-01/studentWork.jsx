a2a6ccea2ee3956f06bf1eeac5cbe7614210641c
An Nguyen <anqnguyen3@gmail.com>
2026-04-10 22:46:58 -0500

    lesson01 weekly

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,22 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'An Nguyen';
+  const age = 21;
+  const hobbies = ['Reading', 'Gaming', 'Coding', 'Fishing'];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        Hi! My name is {name} and I am {age} years old.
+      </p>
+      <h2>My Hobbies</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
